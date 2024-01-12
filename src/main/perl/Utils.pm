1abffeabc6f74763db2b54c3bada3f6deca1c0d9
Yegor Bugayenko <yegor256@gmail.com>
2024-01-12 17:13:29 +0300

    #16 mkdir

--- src/main/perl/Utils.pm
+++ src/main/perl/Utils.pm
@@ -26,6 +26,8 @@ package Utils;
 use strict;
 use warnings;
 use Exporter;
+use File::Basename;
+use File::Path qw( make_path );
 
 our @ISA= qw( Exporter );
 our @EXPORT = qw( fread fwrite );
@@ -33,7 +35,7 @@ our @EXPORT = qw( fread fwrite );
 # Read file content.
 sub fread {
   my ($path) = @_;
-  open(my $h, '<', $path) or die('Cannot open file: ' . $path);
+  open(my $h, '<', $path) or die("Cannot open file: '$path'");
   my $content; { local $/; $content = <$h>; }
   return $content;
 }
@@ -41,7 +43,8 @@ sub fread {
 # Save content to file.
 sub fwrite {
   my ($path, $content) = @_;
-  open(my $f, '>', $path) or die('Cannot open file for writing: ' . $path);
+  make_path(dirname($path));
+  open(my $f, '>', $path) or die("Cannot open file for writing: '$path'");
   print $f $content;
   close($f);
 }
