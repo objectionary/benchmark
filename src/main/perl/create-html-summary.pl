bced0a66df7a5befb095ad601a79c936b0c58f12
Yegor Bugayenko <yegor256@gmail.com>
2025-06-26 14:25:27 +0300

    Update eo-maven-plugin version to ${eo.version}.

--- src/main/perl/create-html-summary.pl
+++ src/main/perl/create-html-summary.pl
@@ -2,17 +2,6 @@
 # SPDX-FileCopyrightText: Copyright (c) 2023-2025 Objectionary.com
 # SPDX-License-Identifier: MIT
 
-# The above copyright notice and this permission notice shall be included
-# in all copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 use strict;
 use warnings;
 use Env;
@@ -39,7 +28,6 @@ sub inject {
   my $safe = $pre;
   $safe =~ s/</&lt;/g;
   $safe =~ s/>/&gt;/g;
-  $safe =~ s/\$/\\\$/g;
   my $show = fread('src/main/html/show.html');
   $show =~ s/\{snippet\}/$snippet/g;
   $show =~ s/\{pre\}/$safe/g;
@@ -131,6 +119,7 @@ chomp $javac_version;
 $html = inject($html, 'version-javac', $javac_version);
 $html = inject($html, 'eo-version', $ENV{EO_VERSION} || '');
 $html = inject($html, 'jeo-version', $ENV{JEO_VERSION} || '');
+$html = inject($html, 'phino-version', $ENV{PHINO_VERSION} || '');
 $html = inject($html, 'jd-version', $ENV{JD_VERSION} || '');
 
 fwrite('html/summary.html', timed($html));
