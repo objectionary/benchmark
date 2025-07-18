fd657230505bb42e0bc96357b56f05eeb238543e
Yegor Bugayenko <yegor256@gmail.com>
2025-07-18 05:49:21 +0300

    Update method signature in rules file to match code changes.

--- src/test/rewrite/Foo.java
+++ src/test/rewrite/Foo.java
@@ -26,12 +26,4 @@ public class Foo {
         System.out.printf("result = %d\n", r);
     }
 
-    private static String first(Integer x) {
-        return Integer.toString(x) + " hey";
-    }
-
-    private static void second(Integer x, Consumer<String> c) {
-        c.accept(first(x));
-    }
-
 }
