8297fb11211c208cd7964813dd72b595c4fc60e6
maxonfjvipon <mtrunnikov@gmail.com>
2025-07-30 17:29:38 +0300

    no names and types

--- src/test/Foo.java
+++ src/test/Foo.java
@@ -3,8 +3,6 @@
  * SPDX-License-Identifier: MIT
  */
 
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
 import java.util.stream.IntStream;
 
 public class Foo {
@@ -13,37 +11,10 @@ public class Foo {
         final int[] input = { 1, 2, 4, 8, 16 };
         final long r = IntStream.of(input)
             .boxed()
-            .map(String::valueOf)
-            .filter(Foo::isNotEmpty)
-            .map(x -> Integer.valueOf(x) + 1)
-            .filter(x -> x > 8)
             .map(x -> x + 1)
-            .filter(x -> x > 8)
-            .mapMulti(
-                (BiConsumer<Integer, Consumer<Integer>>) (x, consumer) -> {
-                    if (!bobobo(x)) {
-                        return;
-                    }
-                    consumer.accept(x);
-                }
-            )
-            .mapMulti(
-                (BiConsumer<Integer, Consumer<Integer>>) (x, consumer) -> {
-                    if (bobobo(x)) {
-                        consumer.accept(x);
-                    }
-                }
-            )
-            .mapToLong(x -> (long) x)
+            .map(Integer::doubleValue)
+            .mapToLong(x -> x.longValue())
             .sum();
         System.out.printf("%d\n", r);
-    }
-
-    private static boolean bobobo(Integer i) {
-        return i > 10;
-    }
-
-    private static boolean isNotEmpty(Object str) {
-        return !((String) str).isEmpty();
     }
 }
